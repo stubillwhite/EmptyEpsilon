a20ce5f88e00a9f09ca584b7ddd5266a20194971
tdelc <root@debian>
2020-08-28 09:56:14 +0200

    Correction single pilot view port 3d

--- src/screens/crew1/singlePilotScreen.cpp
+++ src/screens/crew1/singlePilotScreen.cpp
@@ -7,29 +7,22 @@
 #include "gui/gui2_element.h"
 
 #include "screenComponents/viewport3d.h"
+#include "screenComponents/viewportMainScreen.h"
 
 #include "screenComponents/alertOverlay.h"
 
 SinglePilotScreen::SinglePilotScreen(GuiContainer* owner)
 : GuiOverlay(owner, "SINGLEPILOT_SCREEN", colorConfig.background)
 {
-    // Create a 3D viewport behind everything, to serve as the right-side panel
-    viewport = new GuiViewport3D(this, "3D_VIEW");
-    viewport->setPosition(1000, 0, ATopLeft)->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
-
-    // Create left panel for controls.
-    left_panel = new GuiElement(this, "LEFT_PANEL");
-    left_panel->setPosition(0, 0, ATopLeft)->setSize(1000, GuiElement::GuiSizeMax);
-    
-    // single pilot
-    single_pilot_view = new SinglePilotView(left_panel, my_spaceship);
-    single_pilot_view->setPosition(0, 0, ATopLeft)->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
-
     background_crosses = new GuiOverlay(this, "BACKGROUND_CROSSES", sf::Color::White);
     background_crosses->setTextureTiled("gui/BackgroundCrosses");
 
     // Render the alert level color overlay.
     (new AlertLevelOverlay(this));
+
+    // single pilot
+    single_pilot_view = new SinglePilotView(this, my_spaceship);
+    single_pilot_view->setPosition(0, 0, ATopLeft)->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
 }
 
 void SinglePilotScreen::onDraw(sf::RenderTarget& window)
