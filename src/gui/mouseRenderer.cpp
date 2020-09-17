ff0cec582529baa856abfe57ee7dc1a0fe913105
tdelc <root@debian>
2020-09-17 10:01:09 +0200

    Move mouse to corner of screen

--- src/gui/mouseRenderer.cpp
+++ src/gui/mouseRenderer.cpp
@@ -5,6 +5,10 @@ MouseRenderer::MouseRenderer()
 : Renderable(mouseLayer)
 {
     visible = true;
+
+    // Initialize mouse position
+    sf::Vector2f mouse_init(0.0, 2);
+    InputHandler::setMousePos(mouse_init);
 }
 
 void MouseRenderer::render(sf::RenderTarget& window)
