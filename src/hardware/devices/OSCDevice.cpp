3770e98e0baf487ab0861bae059b65c91cda42ad
Tom Bull <tom@tombull.com>
2023-05-28 15:13:46 +0100

    Even more logging

--- src/hardware/devices/OSCDevice.cpp
+++ src/hardware/devices/OSCDevice.cpp
@@ -58,6 +58,7 @@ bool OSCDevice::configure(std::unordered_map<string, string> settings)
 //Set a hardware channel output. Value is 0.0 to 1.0 for no to max output.
 void OSCDevice::setChannelData(int channel, float value)
 {
+    LOG(INFO) << "Preparing OSC packet for: " << osc_addresses[channel] << ", with value: " << value;
     uint8_t buffer[maximum_udp_packet_size] = {};
     OSCPP::Client::Packet packet(buffer, maximum_udp_packet_size);
     const char* osc_address = osc_addresses[channel].c_str();
